e12100d998c5e2a9100ed348ac728ef57b7f710f
victorbdr <victor.ono.badaro@gmail.com>
2022-10-17 03:48:13 -0300

    feat: delete function functional

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -61,5 +61,28 @@ async function urlRedirect(req, res) {
     res.sendStatus(500);
   }
 }
+async function deletOption(req, res) {
+  const { id } = req.params;
+  const { userId } = res.locals;
+  try {
+    const deleteLink = await db.query(`SELECT * FROM links WHERE id = $1`, [
+      id,
+    ]);
+    if (!deleteLink.rows.length) {
+      return res.sendStatus(404);
+    }
+    if (deleteLink.rows[0].userId !== userId) {
+      return res.sendStatus(401);
+    }
+    await db.query(`DELETE FROM links WHERE id=$1 AND "userId"=$2`, [
+      id,
+      userId,
+    ]);
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
 
-export { linkShortener, getRequestedLink, urlRedirect };
+export { linkShortener, getRequestedLink, urlRedirect, deletOption };
--- routes/urlRoute.js
+++ routes/urlRoute.js
@@ -3,6 +3,7 @@ import {
   linkShortener,
   urlRedirect,
   getRequestedLink,
+  deletOption,
 } from "../controllers/urlController.js";
 import { tokenValidate } from "../middlewares/authMiddleware.js";
 import { LinkValidation } from "../middlewares/ulrMiddleware.js";
@@ -11,4 +12,5 @@ const linkRoute = Router();
 linkRoute.post("/urls/shorten", tokenValidate, LinkValidation, linkShortener);
 linkRoute.get("/urls/:id", getRequestedLink);
 linkRoute.get("/urls/open/:shortUrl", urlRedirect);
+linkRoute.delete("/urls/:id", tokenValidate, deletOption);
 export default linkRoute;
